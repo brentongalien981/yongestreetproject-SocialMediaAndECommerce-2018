5da4380304d7be2598aa5b0a88c01b8b3f6c471f
ops <odox700@gmail.com>
2018-07-12 18:07:19 -0400

    - Set the event-listeners for ThreeColumnedPage.js (ThreeColumnedPageEventListeners.js)

---
+++ app/public/js/cn-event-listeners/ThreeColumnedPageEventListeners.js
@@ -0,0 +1,54 @@
+// import CreateVideoPageController from "../js-controllers/CreateVideoPageController";
+
+class ThreeColumnedPageEventListeners {
+
+    static implement(handlerObj) {
+
+        // if (handlerObj instanceof CreateVideoPageController) {
+        //     $(window).resize(function () {
+        //         handlerObj.setLeftColHeight();
+
+        //         // setRightCol();
+        //         // handlerObj.setRightColHeight();
+
+        //         // handlerObj.setCenterCol();
+        //     });
+        // } else {
+        //     cnLog("Argument: handlerObj is not an instance of class: CreateVideoPageController.");
+        // }
+
+        $(window).resize(function () {
+            handlerObj.setLeftColHeight();
+
+            // setRightCol();
+            // handlerObj.setRightColHeight();
+
+            // handlerObj.setCenterCol();
+        });
+
+
+
+    }
+
+
+    /**
+     * Call this method instead of implement() if the calling class
+     * doesn't want to handle the event.
+     * @param {CreateVideoPageController} delegator 
+     */
+    static handle(delegator) {
+
+        delegator.setLeftColHeight = this.setLeftColHeight;
+        this.implement(delegator);
+
+    }
+
+
+    static setLeftColHeight() {
+        // $("#cn-left-col").height($(this).outerHeight());
+        $(this.view.parts.cnLeftCol.node).height($(window).outerHeight());
+    }
+
+}
+
+export { ThreeColumnedPageEventListeners as default }
--- app/public/js/js-controllers/CreateVideoPageController.js
+++ app/public/js/js-controllers/CreateVideoPageController.js
@@ -1,6 +1,7 @@
 import PageController from "./PageController.js";
 import CreateVideoPage from "../cn-components/CreateVideoPage.js";
 import VideoUserPlaylistsPlugInController from "./VideoUserPlaylistsPlugInController.js";
+import ThreeColumnedPageEventListeners from "../cn-event-listeners/ThreeColumnedPageEventListeners.js";
 
 class CreateVideoPageController extends PageController {
 
@@ -9,20 +10,30 @@ class CreateVideoPageController extends PageController {
 
         this.view = new CreateVideoPage();
 
+        this.implementEventListeners();
+        
         this.videoUserPlaylistsController.view.appendTo(this.view.parts.cnLeftCol);
         this.videoUserPlaylistsController.read();
+
     }
+
+    implementEventListeners() {
+        // this.eventListenerObj = new ThreeColumnedPageEventListeners();
+        // ThreeColumnedPageEventListeners.handle(this);
+        ThreeColumnedPageEventListeners.implement(this);
+    }
+
 
     /**
      * @override
      */
     initExtentionalControllers() {
         this.videoUserPlaylistsController = new VideoUserPlaylistsPlugInController();
-        
+
     }
 }
 
-export { CreateVideoPageController as default}
+export { CreateVideoPageController as default }
 
 
 $(document).ready(function () {
